5a3085365ffee9a14d0f3bef2154f8ef0a2ab760
Jack <545646733@qq.com>
2018-10-22 22:43:20 +0800

    加入创建数据库和表的各种配置.

---
+++ app/src/main/java/com/example/administrator/windyweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.example.administrator.windyweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2018-10-22.
+ */
+
+public class Province extends DataSupport{
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
